6dedac9ca099fa307ce35555deda536d31520a56
xiaoyang07 <xiaoyang07@kuaishou.com>
2025-12-09 13:17:44 +0800

    补充迭代法的去重逻辑

--- src/testSuanfa/linkList/mergeListAndDuplicate.ts
+++ src/testSuanfa/linkList/mergeListAndDuplicate.ts
@@ -28,6 +28,41 @@ function mergeTwoLists(pre: ListNode, list1: ListNode | null, list2: ListNode | 
     }
 };
 
+function iterationMergeTwoList(list1, list2) {
+    const dummy = new ListNode(undefined);
+    let cur = dummy;
+    
+    while (list1 && list2) {
+        // 选择较小的节点
+        if (list1.val <= list2.val) {
+            // 去重：跳过与当前节点值相同的节点
+            if (cur.val !== list1.val) {
+                cur.next = list1;
+                cur = cur.next;
+            }
+            list1 = list1.next;
+        } else {
+            if (cur.val !== list2.val) {
+                cur.next = list2;
+                cur = cur.next;
+            }
+            list2 = list2.next;
+        }
+    }
+    
+    // 处理剩余节点
+    let remain = list1 || list2;
+    while (remain) {
+        if (cur.val !== remain.val) {
+            cur.next = remain;
+            cur = cur.next;
+        }
+        remain = remain.next;
+    }
+    
+    return dummy.next;
+}
+
 const arr = [1,2,2,4,5,7,8];
 const arr2 = [2,3,5,8,9];
 function getListNode(arr: number[]) {
@@ -54,6 +89,7 @@ function printList(head: ListNode) {
 const head = getListNode(arr);
 const head2 = getListNode(arr2);
 const resHead = mergeTwoLists(null, head, head2);
+// const resHead = iterationMergeTwoList(head, head2);
 printList(resHead);
 
 

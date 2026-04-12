fe1ce58b3c0200695623f047a0a8d2d1810682d0
xiaoyang07 <xiaoyang07@kuaishou.com>
2026-04-13 00:44:16 +0800

    补充爬楼梯超大输入解决方案

--- src/testSuanfa/dynamicPlan/ClimbStairs.ts
+++ src/testSuanfa/dynamicPlan/ClimbStairs.ts
@@ -1,10 +1,13 @@
 export {};
-function climbStairs(n: number): number {
-    if (n <= 2) return n;
-    let pre1 = 1;
-    let pre2 = 2;
-    let res = 0;
-    for (let i = 3; i <= n; i++) {
+// bigInt解决 输入9999问题
+function climbStairs(input: number): bigint {
+    if (input <= 2) {
+        return input == 1 ? 1n : 2n;
+    }
+    let pre1 = 1n;
+    let pre2 = 2n;
+    let res = 0n;
+    for (let i = 3; i <= input; i++) {
         res = pre1 + pre2;
         pre1 = pre2;
         pre2 = res;
@@ -13,4 +16,4 @@ function climbStairs(n: number): number {
 };
 
 const res = climbStairs(9999);
-console.log('yizhi res is ', res);
+console.log('yizhi res is ', res.toString());
